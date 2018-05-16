e9a9e0824378d99f0764cf28de5dcf92e4b6d335
Declan McKelvey-Hembree <dmckelvey-hembree@thisisfusion.com>
2018-05-15 21:43:20 -0500

    init: first twitter hookup

---
+++ index.js
@@ -0,0 +1,33 @@
+
+var Twitter = require('twitter');
+require('dotenv').config();
+
+var client = new Twitter({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+
+function getRealTrumpTweets(callback){
+	client.get('/statuses/user_timeline',
+	{
+		screen_name: 'realDonaldTrump',
+		exclude_replies: true,
+		tweet_mode: 'extended'
+
+	},
+	function(error, tweets, response){
+		for(tweet of tweets){
+			//strip URLs, not useful for dogefying
+			tweet.full_text = tweet.full_text.replace(/http[^\s]*/g, '');
+			callback(tweet);
+		}
+	})
+}
+
+function convertToDoge(tweet){
+	console.log(tweet.full_text);
+}
+
+getRealTrumpTweets(convertToDoge);

419d5501077e4a14c7e4d041b518a0438aa3d129
Declan McKelvey-Hembree <dmckelvey-hembree@thisisfusion.com>
2018-05-21 19:24:21 -0500

    feature: wrote twitter response function

--- index.js
+++ index.js
@@ -10,19 +10,31 @@ var client = new Twitter({
   access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
+var lastID = null;
+
 function getRealTrumpTweets(callback){
-	client.get('/statuses/user_timeline',
-	{
+	var params = {
 		screen_name: 'realDonaldTrump',
 		exclude_replies: true,
 		tweet_mode: 'extended'
-
-	},
-	function(error, tweets, response){
-		for(tweet of tweets){
-			//strip URLs, not useful for dogefying
-			tweet.full_text = tweet.full_text.replace(/http[^\s]*/g, '');
-			callback(tweet);
+	};
+	if(lastID != null){
+		params.since_id = lastID;
+	}
+	client.get('/statuses/user_timeline', params, function(error, tweets, response){
+		if(error){
+			console.log("Attempting to get real trump twets encountered error:");
+			console.log(error);
+		}
+		if(tweets.length > 0){
+			lastID = tweets[0].id;
+				for(tweet of tweets){
+				console.log(tweet);
+				//strip URLs, not useful for dogefying
+				tweet.full_text = tweet.full_text.replace(/http[^\s]*/g, '');
+				callback(tweet);
+				return;
+			}
 		}
 	})
 }
@@ -33,6 +45,23 @@ function convertToDoge(tweet){
 	console.log("->");
 	console.log(shibeVersion);
 	console.log("------");
+	var params = {
+		status: shibeVersion,
+		in_reply_to_status_id: tweet.id_str,
+		auto_populate_reply_metadata: true
+	};
+	console.log(params);
+	client.post('/statuses/update', params, function (error, tweet, response){
+			if(error){
+				console.log("Tweet post attempt error:");
+				console.log(error);
+			}else{
+				console.log("It worked!");
+				console.log(response);
+			}
+	})
 }
 
-getRealTrumpTweets(convertToDoge);
+//setInterval(function(){
+	getRealTrumpTweets(convertToDoge);
+//}, 6000);
